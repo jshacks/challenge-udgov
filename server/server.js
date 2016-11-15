b4328796a604e356a48c15435aa0c54a43c82144
Alexandru Albu <afalex89@gmail.com>
2016-11-15 21:59:48 +0200

    - changed server port to 80

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port        = process.env.PORT || 8080;         // set our port
+var port        = process.env.PORT || 80;         // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
